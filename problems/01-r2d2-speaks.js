fc5a217e92ba2f3e4cccc1aff94f40c02640bc5b
zacharybaca <zachary.baca@msn.com>
2023-08-10 16:29:48 -0500

    completed r2d2 problem

--- problems/01-r2d2-speaks.js
+++ problems/01-r2d2-speaks.js
@@ -1,7 +1,7 @@
 /*
     Write a function r2d2Speaks that takes in a series of 0s and 1s.
     The function should console.log a 'beep' for a 0 followed by a pause of 400 ms and a 'boop' for a 1 followed by 800 ms.
-    
+
     let code = [0, 1, 1, 0];
     r2d2Speaks(code);
         print 'beep'
@@ -18,20 +18,22 @@ function r2d2Speaks(code, index = 0) {
    if (index === code.length) {
        return false;
    }
+
    if (code[index] === 0) {
-       setTimeout(() => {
-           console.log('beep');
-       }, 400)
+    setTimeout(() => {
+        console.log('beep');
+        return r2d2Speaks(code, index + 1);
+    }, 400)
    } else if (code[index] === 1) {
-       setTimeout(() => {
-           console.log('boop');
-       }, 800)
+    setTimeout(() => {
+        console.log('boop');
+        return r2d2Speaks(code, index + 1);
+    }, 800)
    }
-   return r2d2Speak(code, index + 1);
 }
 
-let code = [0, 1, 1, 0];
-r2d2Speaks(code);
+// let code = [0, 1, 1, 0];
+// r2d2Speaks(code);
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
