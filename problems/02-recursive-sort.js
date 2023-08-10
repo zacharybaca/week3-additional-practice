5d7c2d5d40b550f20367f8b0c12b42bcb0821891
Zachary Baca <zachary.baca@msn.com>
2023-08-10 14:43:28 -0500

    started on problem

--- problems/02-recursive-sort.js
+++ problems/02-recursive-sort.js
@@ -9,9 +9,17 @@
     console.log(sort([14, 5, 10, 6, 3, 4, 21, 16, 9])); // prints [ 3, 4, 5, 6, 9, 10, 14, 16, 21 ]
 */
 
-function sort(nums) {
-    // Your code here
+function sort(nums, newArr = [], index = 0) {
+   if (nums.length <= 1) {
+       newArr.push(nums[index]);
+       return newArr;
+   }
 }
+
+console.log(sort([])) // prints []
+console.log(sort([9])) // prints [9]
+console.log(sort([5, 4, 3, 2, 1])) // prints [1, 2, 3, 4, 5]
+console.log(sort([14, 5, 10, 6, 3, 4, 21, 16, 9])); // prints [ 3, 4, 5, 6, 9, 10, 14, 16, 21 ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
